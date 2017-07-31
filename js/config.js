375d2b3a9299bd932c47fa542f79456b3e817400
Bernard Adanlessossi <adanlessossi@hotmail.com>
2017-07-31 07:23:08 +0200

    First commit

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'eu-central-1_9wkKpUXla', // e.g. eu-central-1_EcnHjIGCy
+        userPoolClientId: '5m06t0uom5pgeecb0qq7c5v2p6', // e.g. 1g52k6jkclolrckli37vl49odg
+        region: 'eu-central-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: '' // e.g. https://8nh8e7xt4i.execute-api.eu-central-1.amazonaws.com/prod',
+    }
+};
---
+++ js/main.js
@@ -0,0 +1,7 @@
+"use strict";
+! function() {
+    var n = $("html"),
+        t = function() { $(".btn-menu").on("click", function(t) { t.preventDefault(), n.toggleClass("menu-opened") }) },
+        e = function() { t() };
+    e()
+}();
